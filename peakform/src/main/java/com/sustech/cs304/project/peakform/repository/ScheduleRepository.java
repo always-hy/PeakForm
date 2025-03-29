8a3b668793295b79f44b47516a3db9ab81240e97
fanxychild <100819203+Layheng-Hok@users.noreply.github.com>
2025-03-29 16:48:40 +0800

    Init GymSchedule table

--- peakform/src/main/java/com/sustech/cs304/project/peakform/repository/ScheduleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sustech.cs304.project.peakform.repository;
-
-import com.sustech.cs304.project.peakform.domain.Schedule;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
-}
