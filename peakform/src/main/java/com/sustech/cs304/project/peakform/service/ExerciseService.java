55958b1d7df0b998f5930ff451b0e39c652ab207
Jouwy <132906518+Jouwy@users.noreply.github.com>
2025-04-26 21:59:55 +0800

    update ExerciseList (cache)

--- peakform/src/main/java/com/sustech/cs304/project/peakform/service/ExerciseService.java
+++ peakform/src/main/java/com/sustech/cs304/project/peakform/service/ExerciseService.java
@@ -3,6 +3,7 @@ package com.sustech.cs304.project.peakform.service;
 import com.sustech.cs304.project.peakform.dto.ExerciseListResponse;
 import com.sustech.cs304.project.peakform.repository.ExerciseRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,6 +14,7 @@ public class ExerciseService {
 
     private final ExerciseRepository exerciseRepository;
 
+    @Cacheable(value = "exerciseList")
     public List<ExerciseListResponse> getAllExercises() {
         return exerciseRepository.findAll().stream()
                 .map(ex -> new ExerciseListResponse(
