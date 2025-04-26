10aecb6d0a165d5abc98f576f488bbd4c6dfab70
fanxychild <100819203+Layheng-Hok@users.noreply.github.com>
2025-04-27 00:35:35 +0800

    Containerize backend

--- peakform/src/main/java/com/sustech/cs304/project/peakform/PeakFormApplication.java
+++ peakform/src/main/java/com/sustech/cs304/project/peakform/PeakFormApplication.java
@@ -1,6 +1,5 @@
 package com.sustech.cs304.project.peakform;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -11,20 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableCaching
 public class PeakFormApplication {
 
-	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		System.setProperty("spring.datasource.username", dotenv.get("DB_USERNAME"));
-		System.setProperty("spring.datasource.password", dotenv.get("DB_PASSWORD"));
-		System.setProperty("spring.mail.username", dotenv.get("MAIL_USERNAME"));
-		System.setProperty("spring.mail.password", dotenv.get("MAIL_PASSWORD"));
-		System.setProperty("spring.security.oauth2.client.registration.google.client-id", dotenv.get("GOOGLE_CLIENT_ID"));
-		System.setProperty("spring.security.oauth2.client.registration.google.client-secret", dotenv.get("GOOGLE_CLIENT_SECRET"));
-		System.setProperty("spring.security.oauth2.client.registration.facebook.client-id", dotenv.get("FB_CLIENT_ID"));
-		System.setProperty("spring.security.oauth2.client.registration.facebook.client-secret", dotenv.get("FB_CLIENT_SECRET"));
-		System.setProperty("spring.security.oauth2.client.registration.github.client-id", dotenv.get("GITHUB_CLIENT_ID"));
-		System.setProperty("spring.security.oauth2.client.registration.github.client-secret", dotenv.get("GITHUB_CLIENT_SECRET"));
-		System.setProperty("alipay.private-key", dotenv.get("ALIPAY_PRIVATE_KEY"));
-
-		SpringApplication.run(PeakFormApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PeakFormApplication.class, args);
+    }
 }
