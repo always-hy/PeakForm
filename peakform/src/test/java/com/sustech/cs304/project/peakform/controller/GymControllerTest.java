21586e3ab1677892289f9cf34612f34437537b7d
fanxychild <100819203+Layheng-Hok@users.noreply.github.com>
2025-05-17 23:14:21 +0800

    Update GymControllerTest.java

--- peakform/src/test/java/com/sustech/cs304/project/peakform/controller/GymControllerTest.java
+++ peakform/src/test/java/com/sustech/cs304/project/peakform/controller/GymControllerTest.java
@@ -45,11 +45,46 @@ public class GymControllerTest extends AbstractTestContainerConfig {
                 .body("[0].description", equalTo("A gym focused on weightlifting and strength training."))
                 .body("[0].contact", equalTo("123-456-7890"))
                 .body("[0].gymCoverPhoto", matchesRegex("https://storage.googleapis.com/peakform-3d589.firebasestorage.app/gym-photo/1/1-1.webp\\?.*"));
+
+        given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get("/gyms")
+                .then()
+                .statusCode(200)
+                .body(".", hasSize(3))
+                .body("[0].gymId", equalTo(1))
+                .body("[0].gymName", equalTo("AlphaLand"))
+                .body("[0].startTime", equalTo("09:00:00"))
+                .body("[0].endTime", equalTo("21:00:00"))
+                .body("[0].location", equalTo("123 Fitness St, City Center"))
+                .body("[0].description", equalTo("A gym focused on weightlifting and strength training."))
+                .body("[0].contact", equalTo("123-456-7890"))
+                .body("[0].gymCoverPhoto", matchesRegex("https://storage.googleapis.com/peakform-3d589.firebasestorage.app/gym-photo/1/1-1.webp\\?.*"));
     }
 
     @Test
     void getGymSuccessTest() {
         List<String> loginResponse = LoginUtils.login("prakbunlong53@gmail.com", "1");
+
+        given()
+                .contentType(ContentType.JSON)
+                .sessionId(loginResponse.get(1))
+                .queryParam("userUuid", loginResponse.get(0))
+                .when()
+                .get("/gyms/1")
+                .then()
+                .statusCode(200)
+                .body("gymId", equalTo(1))
+                .body("gymName", equalTo("AlphaLand"))
+                .body("startTime", equalTo("09:00:00"))
+                .body("endTime", equalTo("21:00:00"))
+                .body("location", equalTo("123 Fitness St, City Center"))
+                .body("description", equalTo("A gym focused on weightlifting and strength training."))
+                .body("contact", equalTo("123-456-7890"))
+                .body("gymPhotos", hasSize(6))
+                .body("gymPhotos[0]", matchesRegex("https://storage.googleapis.com/peakform-3d589.firebasestorage.app/gym-photo/1/1-1.webp\\?.*"));
+
         given()
                 .contentType(ContentType.JSON)
                 .sessionId(loginResponse.get(1))
