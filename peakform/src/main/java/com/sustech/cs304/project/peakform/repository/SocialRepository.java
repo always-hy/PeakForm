66a06ac9aa4084d76f26c59d447c76209c65d5f6
fanxychild <100819203+Layheng-Hok@users.noreply.github.com>
2025-05-24 22:50:27 +0800

    Allow user to follow others

--- peakform/src/main/java/com/sustech/cs304/project/peakform/repository/SocialRepository.java
+++ peakform/src/main/java/com/sustech/cs304/project/peakform/repository/SocialRepository.java
@@ -1,13 +1,17 @@
 package com.sustech.cs304.project.peakform.repository;
 
 import com.sustech.cs304.project.peakform.domain.Social;
+import com.sustech.cs304.project.peakform.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface SocialRepository extends JpaRepository<Social, Long> {
+
     List<Social> findByFollower_UserUuid(UUID userUuid);
 
     List<Social> findByFollowing_UserUuid(UUID userUuid);
+
+    Boolean existsByFollowerAndFollowing(User follower, User following);
 }
