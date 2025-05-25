7e90bb281991d5233ad7593591298bed5d5b24b6
fanxychild <100819203+Layheng-Hok@users.noreply.github.com>
2025-05-25 21:53:38 +0800

    Init UserRecord table and add endpoint to get UserRecord by User UUID

---
+++ peakform/src/main/java/com/sustech/cs304/project/peakform/controller/UserRecordController.java
@@ -0,0 +1,45 @@
+package com.sustech.cs304.project.peakform.controller;
+
+import com.sustech.cs304.project.peakform.domain.User;
+import com.sustech.cs304.project.peakform.domain.UserRecord;
+import com.sustech.cs304.project.peakform.dto.UserRecordResponse;
+import com.sustech.cs304.project.peakform.repository.UserRecordRepository;
+import com.sustech.cs304.project.peakform.repository.UserRepository;
+import com.sustech.cs304.project.peakform.service.UserRecordService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/records")
+public class UserRecordController {
+
+    private final UserRepository userRepository;
+    private final UserRecordRepository userRecordRepository;
+
+    private final UserRecordService userRecordService;
+
+    @GetMapping()
+    @PreAuthorize("#userUuid.toString() == authentication.principal.userUuid.toString()")
+    public ResponseEntity<UserRecordResponse> getUserRecord(@RequestParam("userUuid") UUID userUuid) {
+        Optional<User> userOptional = userRepository.findByUserUuid(userUuid);
+        if (userOptional.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        Optional<UserRecord> userRecordOptional = userRecordRepository.findByUser_UserUuid(userUuid);
+        if (userRecordOptional.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(userRecordService.getUserRecord(userUuid));
+    }
+}
--- peakform/src/main/java/com/sustech/cs304/project/peakform/repository/UserRecordRepository.java
+++ peakform/src/main/java/com/sustech/cs304/project/peakform/repository/UserRecordRepository.java
@@ -3,5 +3,10 @@ package com.sustech.cs304.project.peakform.repository;
 import com.sustech.cs304.project.peakform.domain.UserRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public interface UserRecordRepository extends JpaRepository<UserRecord, Long> {
+
+    Optional<UserRecord> findByUser_UserUuid(UUID userUuid);
 }
