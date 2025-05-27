93559169f3af558ccc1e8cae4834c514f4b3a78d
Barracudapi <132427435+Barracudapi@users.noreply.github.com>
2025-05-27 13:59:14 +0800

    Social/ Leaderboard Feature

---
+++ peakform-frontend/src/app/social/page.js
@@ -0,0 +1,8 @@
+import Image from "next/image";
+
+import React from "react";
+import SocialLeaderboard from "@/components/Social/SocialLeaderboard";
+
+export default function Page() {
+  return <SocialLeaderboard />;
+}
