112d8539e72b749fbb9a3f47936c19121125f5c0
Barracudapi <132427435+Barracudapi@users.noreply.github.com>
2025-05-29 02:13:51 +0800

    vercel url config fix

--- peakform-frontend/src/config.js
+++ peakform-frontend/src/config.js
@@ -1,3 +1,3 @@
 export const API_URL = "http://35.201.134.240";
-export const FRONTEND_URL = "peak-form-two-vercel-app";
+export const FRONTEND_URL = "peak-form-two.vercel.app";
 // export const API_URL = "http://localhost:8080";
